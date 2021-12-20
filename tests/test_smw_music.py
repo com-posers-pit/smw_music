247547eed2e9b0ec66fd6d5b704cb802000e7838
Thomas A. Werne <werneta@gmail.com>
2021-12-19 19:50:50 -0800

    Add SMB castle theme test

--- tests/test_smw_music.py
+++ tests/test_smw_music.py
@@ -1,8 +1,61 @@
 """SMW Music Module Tests."""
 
-from smw_music import __version__
+###############################################################################
+# Standard library imports
+###############################################################################
+
+import os
+import tempfile
+import pathlib
+
+###############################################################################
+# Library imports
+###############################################################################
+
+import pytest
+
+###############################################################################
+# Project imports
+###############################################################################
+
+from smw_music import music_xml, __version__
+
+
+###############################################################################
+# Fixture definitions
+###############################################################################
+
+
+@pytest.fixture
+def constants():
+    return {"test_dir": pathlib.Path("tests")}
+
+
+###############################################################################
+# Test definitions
+###############################################################################
 
 
 def test_version():
     """Verify correct version number."""
     assert __version__ == "0.0.0"
+
+
+###############################################################################
+
+
+def test_smb_castle(constants):
+    test_dir = constants["test_dir"]
+    fname = test_dir / "dst" / "SMB_Castle_Theme.txt"
+
+    with open(fname, "r") as fobj:
+        target = fobj.readlines()
+
+    for fname in ["SMB_Castle_Theme.mxl", "SMB_Castle_Theme.musicxml"]:
+        fname = test_dir / "src" / fname
+
+        with tempfile.NamedTemporaryFile("r") as fobj:
+            music_xml.convert(fname, fobj.name)
+            written = fobj.readlines()
+
+        assert target == written
