00c845b19e5f216926e98ded361c404c599c1c2a
Thomas A. Werne <werneta@gmail.com>
2021-12-21 23:50:56 -0800

    Add annotation handling
    Closes #1

--- smw_music/music_xml.py
+++ smw_music/music_xml.py
@@ -31,8 +31,19 @@ _MUSIC_XML_DURATION = {
     9: 32,
 }
 
+_Track_Elem = Union["Annotation", "Measure", "Note", "Rest"]
+
+_CRLF = "\r\n"
+
 ###############################################################################
 # Private function definitions
+###############################################################################
+
+
+def _is_measure(elem: music21.stream.Stream) -> bool:
+    return isinstance(elem, music21.stream.Measure)
+
+
 ###############################################################################
 
 
@@ -45,9 +56,30 @@ def _most_common(container: Iterable[_T]) -> _T:
 ###############################################################################
 
 
-class Note:
+class Annotation:
+    def __init__(self, text: str):
+        self.text = text
+
     ###########################################################################
 
+    @classmethod
+    def from_music_xml(
+        cls, elem: music21.expressions.TextExpression
+    ) -> "Annotation":
+        return cls(elem.content)
+
+
+###############################################################################
+
+
+class Measure:
+    pass
+
+
+###############################################################################
+
+
+class Note:
     def __init__(self, name: str, duration: int, octave: int):
         self.name = name
         self.duration = duration
@@ -93,21 +125,30 @@ class Song:
     @classmethod
     def from_music_xml(cls, fname: str) -> "Song":
         metadata = {}
-        parts: List[List[Union[Note, Rest]]] = []
+        parts: List[List[_Track_Elem]] = []
         for elem in music21.converter.parseFile(fname):
             if isinstance(elem, music21.metadata.Metadata):
                 metadata["composer"] = elem.composer
                 metadata["title"] = elem.title
             elif isinstance(elem, music21.stream.Part):
                 parts.append([])
-                notes = parts[-1]
-                for subelem in elem.flatten():
-                    if isinstance(subelem, music21.tempo.MetronomeMark):
-                        metadata["bpm"] = subelem.getQuarterBPM()
-                    if isinstance(subelem, music21.note.Note):
-                        notes.append(Note.from_music_xml(subelem))
-                    if isinstance(subelem, music21.note.Rest):
-                        notes.append(Rest.from_music_xml(subelem))
+                track_elem = parts[-1]
+                for measure in filter(_is_measure, elem):
+                    track_elem.append(Measure())
+
+                    for subelem in measure:
+                        if isinstance(subelem, music21.tempo.MetronomeMark):
+                            metadata["bpm"] = subelem.getQuarterBPM()
+                        if isinstance(subelem, music21.note.Note):
+                            track_elem.append(Note.from_music_xml(subelem))
+                        if isinstance(subelem, music21.note.Rest):
+                            track_elem.append(Rest.from_music_xml(subelem))
+                        if isinstance(
+                            subelem, music21.expressions.TextExpression
+                        ):
+                            track_elem.append(
+                                Annotation.from_music_xml(subelem)
+                            )
 
         return cls(metadata, map(Track, parts))
 
@@ -115,7 +156,7 @@ class Song:
 
     def to_amk(self, fname: str):
         with open(fname, "w", encoding="ascii") as fobj:
-            print(self.amk, end="\r\n", file=fobj)
+            print(self.amk, end=_CRLF, file=fobj)
 
     ###########################################################################
 
@@ -133,43 +174,46 @@ class Song:
         amk.append("}")
         for n, track in enumerate(self.track_list):
             amk.append("")
-            amk.append(f"#{n} w255 t{amk_tempo}")
-            amk.append("@9 v150")
+            amk.append(80 * ";")
+            amk.append("")
+            amk.append(f"#{n} t{amk_tempo}")
             amk.append(track.amk)
 
-        return "\r\n".join(amk)
+        return _CRLF.join(amk)
 
 
 ###############################################################################
 
 
 class Track:
-    def __init__(self, notes: List[Union[Note, Rest]]):
-        self.notes = notes
+    def __init__(self, elems: List[_Track_Elem]):
+        self.elems = elems
 
     @property
     def base_note_length(self) -> int:
-        return _most_common(x.duration for x in self.notes)
+        return _most_common(
+            x.duration for x in self.elems if isinstance(x, (Note, Rest))
+        )
 
     @property
     def base_octave(self) -> int:
         return _most_common(
-            x.octave for x in self.notes if isinstance(x, Note)
+            x.octave for x in self.elems if isinstance(x, Note)
         )
 
     @property
     def amk(self) -> str:
         cur_octave = self.base_octave
         base_dur = self.base_note_length
-        rv = [f"o{cur_octave}", ""]
-        rv.append(f"l{base_dur}")
+        directives = [f"o{cur_octave}"]
+        directives.append(f"l{base_dur}")
 
-        for note in self.notes:
-            if isinstance(note, Rest):
+        for elem in self.elems:
+            if isinstance(elem, Rest):
                 directive = "r"
 
-            if isinstance(note, Note):
-                octave = note.octave
+            if isinstance(elem, Note):
+                octave = elem.octave
 
                 if octave != cur_octave:
                     if octave == cur_octave - 1:
@@ -178,14 +222,30 @@ class Track:
                         directive = ">"
                     else:
                         directive = f"o{octave}"
-                    rv.append(directive)
+                    directives.append(directive)
                     cur_octave = octave
 
-                directive = note.name
+                directive = elem.name
 
-            if note.duration != base_dur:
-                directive += str(note.duration)
+            if isinstance(elem, Measure):
+                directive = _CRLF
 
-            rv.append(directive)
+            if isinstance(elem, Annotation):
+                text = elem.text
+                if text.startswith("AMK:"):
+                    directive = text.removeprefix("AMK:").strip()
+                else:
+                    directive = ""
 
-        return " ".join(rv)
+            try:
+                if elem.duration != base_dur:
+                    directive += str(elem.duration)
+            except AttributeError:
+                # Only notes and rests have .duration attributes
+                pass
+
+            if directive:
+                directives.append(directive)
+
+        lines = " ".join(directives).splitlines()
+        return _CRLF.join(x.strip() for x in lines)
