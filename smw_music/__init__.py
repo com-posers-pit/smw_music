29d51b34fb6592bb6c74af10ade23f7f840cf2d0
Thomas A. Werne <werneta@gmail.com>
2021-12-23 14:18:51 -0800

    Add licensing information

--- smw_music/__init__.py
+++ smw_music/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2021 The SMW Music Python Project Authors <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
 """Top level SMW Music Module."""
 
 __version__ = "0.0.0"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
+# SPDX-FileCopyrightText: 2021 The SMW Music Python Project Authors <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
 """SMW Music Package Tests."""
