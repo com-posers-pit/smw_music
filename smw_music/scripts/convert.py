22bae30b494ca167b01ef239a1464a1784128fe9
Thomas A. Werne <werneta@gmail.com>
2021-12-23 15:46:21 -0800

    Line-wrap too-long SPDX author comments

--- smw_music/scripts/convert.py
+++ smw_music/scripts/convert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2021 The SMW Music Python Project Authors <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
+# SPDX-FileCopyrightText: 2021 The SMW Music Python Project Authors
+# <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
