2461d5441c10b882fd28323cb15b358c1fe34239
Thomas A. Werne <werneta@gmail.com>
2021-12-28 15:50:40 -0800

    Add version bump tool

---
+++ bump_version.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+#
+# SPDX-FileCopyrightText: 2021 The SMW Music Python Project Authors
+# <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+"""Project version bump tool."""
+
+###############################################################################
+# Standard Library imports
+###############################################################################
+
+import argparse
+import glob
+import re
+import sys
+import typing
+
+###############################################################################
+# Private function definitions
+###############################################################################
+
+
+def _bump_version(version):
+    key = "__version__ = "
+    _overwrite("smw_music/__init__.py", key, f'{key}"{version}"')
+
+    key = "assert __version__ == "
+    _overwrite("tests/test_smw_music.py", key, f'{key}"{version}"')
+
+    key = "; MusicXML->AMK v"
+    repl = f"{key}{version}"
+    for fname in glob.iglob("tests/dst/*.txt"):
+        _overwrite(fname, key, repl)
+
+
+###############################################################################
+
+
+def _overwrite(fname: str, key: str, repl: str):
+    with open(fname, "r+") as fobj:
+        contents = fobj.readlines()
+        contents = [re.sub(f"{key}.*", repl, x) for x in contents]
+        fobj.seek(0)
+        fobj.truncate()
+        fobj.writelines(contents)
+
+
+###############################################################################
+# API function definitions
+###############################################################################
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    parser = argparse.ArgumentParser("Version Update Tool")
+    parser.add_argument("version", type=str, help="New Version")
+
+    args = parser.parse_args(args)
+
+    _bump_version(args.version)
+
+
+###############################################################################
+# Entrypoint
+###############################################################################
+
+if __name__ == "__main__":
+    main()
