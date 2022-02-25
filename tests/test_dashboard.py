b20970e778f2f128f7f60528c7bc19d739795574
Thomas A. Werne <werneta@gmail.com>
2022-02-24 23:00:26 -0800

    Finish volume slider tests

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -10,6 +10,7 @@
 ###############################################################################
 
 import pathlib
+import re
 import sys
 
 ###############################################################################
@@ -93,13 +94,15 @@ def _set_files(
 ###############################################################################
 
 
-def _set_mp_slider(qtbot, dut: Dashboard, ticks: int) -> None:
-    widget = dut._controller._dynamics._sliders["MP"]._slider
-    qtbot.mouseClick(widget, Qt.MouseButton.LeftButton)
+def _set_dyn_slider(qtbot, dut: Dashboard, dyn: str, ticks: int) -> str:
+    widget = dut._controller._dynamics._sliders[dyn]
+    slider = widget._slider
     key = Qt.Key.Key_Up if ticks > 0 else Qt.Key.Key_Down
     for _ in range(abs(ticks)):
-        qtbot.keyPress(widget, key)
-        qtbot.keyRelease(widget, key)
+        qtbot.keyPress(slider, key)
+        qtbot.keyRelease(slider, key)
+
+    return widget._display.text(), widget._control.text()
 
 
 ###############################################################################
@@ -149,7 +152,6 @@ def setup(request, tmp_path, qtbot, monkeypatch):
         ("measure_numbers.mml", _enable_measure_numbers),
         ("custom_samples.mml", _custom_samples),
         ("custom_percussion.mml", _custom_percussion),
-        ("vanilla.mml", lambda x, y: _set_mp_slider(x, y, 4)),
     ],
     ids=[
         "Vanilla Conversion",
@@ -158,7 +160,6 @@ def setup(request, tmp_path, qtbot, monkeypatch):
         "Measure Numbers",
         "Custom Samples",
         "Custom Percussion",
-        "Set MP volume",
     ],
     indirect=["setup"],
 )
@@ -167,9 +168,122 @@ def test_controls(setup, func, qtbot):
 
     if func is not None:
         func(qtbot, dashboard)
+
     _generate(qtbot, dashboard)
 
+    # Read the result and strip out the build date/time before comparing
     with dst_fname.open(encoding="ascii") as fobj:
         actual = [x for x in fobj.readlines() if not x.startswith("; Built:")]
 
     assert target == actual
+
+
+###############################################################################
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="No qtbot on Windows")
+@pytest.mark.parametrize(
+    "setup, instr, dyn, ticks, expected",
+    [
+        ("vanilla.mml", "Harpsichord", "PPPP", 0, 0x1A),
+        ("vanilla.mml", "Harpsichord", "PPP", 0, 0x26),
+        ("vanilla.mml", "Harpsichord", "PP", 0, 0x40),
+        ("vanilla.mml", "Harpsichord", "P", 0, 0x5A),
+        ("vanilla.mml", "Harpsichord", "MP", 0, 0x73),
+        ("vanilla.mml", "Harpsichord", "MF", 0, 0x8D),
+        ("vanilla.mml", "Harpsichord", "F", 0, 0xB3),
+        ("vanilla.mml", "Harpsichord", "FF", 0, 0xD9),
+        ("vanilla.mml", "Harpsichord", "FFF", 0, 0xE6),
+        ("vanilla.mml", "Harpsichord", "FFFF", 0, 0xF5),
+        ("vanilla.mml", "Harpsichord", "MP", 0, 0x73),
+        ("vanilla.mml", "Harpsichord", "MP", 1, 0x74),
+        ("vanilla.mml", "Harpsichord", "MP", -1, 0x72),
+        ("vanilla.mml", "Harpsichord", "MP", 10, 0x7D),
+        ("vanilla.mml", "Harpsichord", "MP", -10, 0x69),
+        ("vanilla.mml", "Harpsichord", "MP", 120, 235),
+        ("vanilla.mml", "Harpsichord", "MP", -120, 0),
+        ("vanilla.mml", "Harpsichord", "MP", 150, 255),
+        ("vanilla.mml", "Harpsichord", "MF", 0, 141),
+        ("vanilla.mml", "Harpsichord", "MF", 20, 161),
+        ("vanilla.mml", "Harpsichord", "MF", -40, 101),
+        ("vanilla.mml", "Harpsichord", "F", 0, 179),
+        ("vanilla.mml", "Harpsichord", "F", 10, 179),
+        ("vanilla.mml", "Harpsichord", "F", -10, 179),
+        ("vanilla.mml", "Piano", "MP", 0, 0x73),
+        ("vanilla.mml", "Piano", "MP", 1, 0x74),
+        ("vanilla.mml", "Piano", "MP", -1, 0x72),
+        ("vanilla.mml", "Piano", "MP", 10, 0x7D),
+        ("vanilla.mml", "Piano", "MP", -10, 0x69),
+        ("vanilla.mml", "Piano", "MP", 120, 235),
+        ("vanilla.mml", "Piano", "MP", -120, 0),
+        ("vanilla.mml", "Piano", "MP", 150, 255),
+        ("vanilla.mml", "Piano", "MF", 0, 141),
+        ("vanilla.mml", "Piano", "MF", 20, 161),
+        ("vanilla.mml", "Piano", "MF", -40, 101),
+        ("vanilla.mml", "Piano", "F", 0, 179),
+        ("vanilla.mml", "Piano", "F", 10, 189),
+        ("vanilla.mml", "Piano", "F", -10, 169),
+    ],
+    ids=[
+        "Harp. Base PPPP",
+        "Harp. Base PPP",
+        "Harp. Base PP",
+        "Harp. Base P",
+        "Harp. Base MP",
+        "Harp. Base MF",
+        "Harp. Base F",
+        "Harp. Base FF",
+        "Harp. Base FFF",
+        "Harp. Base FFFF",
+        "Harp. MP No change",
+        "Harp. MP +1",
+        "Harp. MP -1",
+        "Harp. MP +10",
+        "Harp. MP -10",
+        "Harp. MP +120",
+        "Harp. MP -120 (saturate at 0)",
+        "Harp. MP +150 (saturate at k55)",
+        "Harp. MF No change",
+        "Harp. MF +20",
+        "Harp. MF -40",
+        "Harp. F No change",
+        "Harp. F +10 (Unused)",
+        "Harp. F -10 (Unused)",
+        "Piano. MP No change",
+        "Piano. MP +1",
+        "Piano. MP -1",
+        "Piano. MP +10",
+        "Piano. MP -10",
+        "Piano. MP +120",
+        "Piano. MP -120 (saturate at 0)",
+        "Piano. MP +150 (saturate at k55)",
+        "Piano. MF No change",
+        "Piano. MF +20",
+        "Piano. MF -40",
+        "Piano. F No change",
+        "Piano. F +10",
+        "Piano. F -10",
+    ],
+    indirect=["setup"],
+)
+def test_dynamics_slider(setup, instr, dyn, ticks, expected, qtbot):
+    dashboard, target, dst_fname = setup
+
+    instrs = dashboard._controller._instruments
+    idxs = instrs.findItems(instr, Qt.MatchFlag.MatchExactly)
+    instrs.setCurrentItem(idxs[0])
+
+    disp, control = _set_dyn_slider(qtbot, dashboard, dyn, ticks)
+
+    _generate(qtbot, dashboard)
+
+    # Pick off only the dynamics settings from the target and generated output
+    target = [x for x in target if x.startswith(f'"{instr}_dyn')][0]
+    with dst_fname.open(encoding="ascii") as fobj:
+        act = [x for x in fobj.readlines() if x.startswith(f'"{instr}_dyn')][0]
+
+    # Confirm the displayed hex value, percent edit value, and MML output are
+    # correct
+    assert int("0" + disp, 16) == expected
+    assert float(control) == pytest.approx(100 * expected / 255, rel=1e-3)
+    assert re.sub(f"_{dyn}=..", f"_{dyn}={expected:02X}", target) == act
