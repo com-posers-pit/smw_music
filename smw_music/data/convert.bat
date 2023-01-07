cf2efb801d3f6a614f4a27c422f91f2d702f9159
Thomas A. Werne <werneta@gmail.com>
2023-01-07 15:57:44 -0800

    Add conversion script templates
    Now playing:
    The Bathtub by GURATRON 1988

---
+++ smw_music/data/convert.bat
@@ -0,0 +1,6 @@
+REM SPDX-FileCopyrightText: 2022 The SMW Music Python Project Authors
+REM <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
+REM
+REM SPDX-License-Identifier: AGPL-3.0-only
+
+AddmusicK.exe -c -norom "${project}.txt"
