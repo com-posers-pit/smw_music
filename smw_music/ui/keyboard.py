326ee323b941f4ea8358861beb6690f316adf7f8
Thomas A. Werne <werneta@gmail.com>
2023-02-26 11:52:49 -0800

    Change keyboard mappings to better match renoise

--- smw_music/ui/keyboard.py
+++ smw_music/ui/keyboard.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022 The SMW Music Python Project Authors
+# SPDX-FileCopyrightText: 2023 The SMW Music Python Project Authors
 # <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
@@ -72,25 +72,57 @@ class Key(QGraphicsRectItem):
 
 ###############################################################################
 
-KEY_TABLE: dict[Qt.Key, str] = {
-    Qt.Key.Key_Q: "c",
-    Qt.Key.Key_2: "c#",
-    Qt.Key.Key_W: "d",
-    Qt.Key.Key_3: "d#",
-    Qt.Key.Key_E: "e",
-    Qt.Key.Key_R: "f",
-    Qt.Key.Key_5: "f#",
-    Qt.Key.Key_T: "g",
-    Qt.Key.Key_6: "g#",
-    Qt.Key.Key_Y: "a",
-    Qt.Key.Key_7: "a#",
-    Qt.Key.Key_U: "b",
+KEY_TABLE: dict[Qt.Key, tuple[str, int]] = {
+    Qt.Key.Key_Z: ("c", 0),
+    Qt.Key.Key_S: ("c#", 0),
+    Qt.Key.Key_X: ("d", 0),
+    Qt.Key.Key_D: ("d#", 0),
+    Qt.Key.Key_C: ("e", 0),
+    Qt.Key.Key_V: ("f", 0),
+    Qt.Key.Key_G: ("f#", 0),
+    Qt.Key.Key_B: ("g", 0),
+    Qt.Key.Key_H: ("g#", 0),
+    Qt.Key.Key_N: ("a", 0),
+    Qt.Key.Key_J: ("a#", 0),
+    Qt.Key.Key_M: ("b", 0),
+    #
+    Qt.Key.Key_Comma: ("c", 1),
+    Qt.Key.Key_L: ("c#", 1),
+    Qt.Key.Key_Period: ("d", 1),
+    Qt.Key.Key_Semicolon: ("d#", 1),
+    Qt.Key.Key_Slash: ("e", 1),
+    #
+    Qt.Key.Key_Q: ("c", 1),
+    Qt.Key.Key_2: ("c#", 1),
+    Qt.Key.Key_W: ("d", 1),
+    Qt.Key.Key_3: ("d#", 1),
+    Qt.Key.Key_E: ("e", 1),
+    Qt.Key.Key_R: ("f", 1),
+    Qt.Key.Key_5: ("f#", 1),
+    Qt.Key.Key_T: ("g", 1),
+    Qt.Key.Key_6: ("g#", 1),
+    Qt.Key.Key_Y: ("a", 1),
+    Qt.Key.Key_7: ("a#", 1),
+    Qt.Key.Key_U: ("b", 1),
+    #
+    Qt.Key.Key_I: ("c", 2),
+    Qt.Key.Key_9: ("c#", 2),
+    Qt.Key.Key_O: ("d", 2),
+    Qt.Key.Key_0: ("d#", 2),
+    Qt.Key.Key_P: ("e", 2),
+    Qt.Key.Key_BracketLeft: ("f", 2),
+    Qt.Key.Key_Equal: ("f#", 2),
+    Qt.Key.Key_BracketRight: ("g", 2),
 }
+
+
+###############################################################################
 
 
 class Keyboard(QGraphicsView):
     keys: dict[str, Key]
     _active: bool
+    _octave: int
 
     ###########################################################################
 
@@ -158,32 +190,38 @@ class Keyboard(QGraphicsView):
             self.active = not self.active
 
         if self.active:
-            if keyval == Qt.Key.Key_Up:
-                self.octave = min(self.octave + 1, 10)
-            if keyval == Qt.Key.Key_Down:
-                self.octave = max(self.octave - 1, 0)
-
-            with suppress(KeyError):
-                key = KEY_TABLE[keyval]
-                self.press_key(key, self.octave)
+            if evt.modifiers() & Qt.KeyboardModifier.KeypadModifier:
+                if keyval == Qt.Key.Key_Asterisk:
+                    self.octave = min(self.octave + 1, 10)
+                if keyval == Qt.Key.Key_Slash:
+                    self.octave = max(self.octave - 1, 0)
+            else:
+                with suppress(KeyError):
+                    key, offset = KEY_TABLE[Qt.Key(keyval)]
+                    self.press_key(key, self.octave, offset)
 
     ###########################################################################
 
     def keyReleaseEvent(self, evt: QKeyEvent) -> None:
         if self.active:
-            with suppress(KeyError):
-                key = KEY_TABLE[evt.key()]
-                self.release_key(key, self.octave)
+            if not (evt.modifiers() & Qt.KeyboardModifier.KeypadModifier):
+                with suppress(KeyError):
+                    key, offset = KEY_TABLE[Qt.Key(evt.key())]
+                    self.release_key(key, self.octave, offset)
 
     ###########################################################################
 
-    def press_key(self, letter: str, octave: int):
+    def press_key(self, letter: str, octave: int, offset: int = 0):
+        octave = max(min(octave + offset, 10), 0)
+
         with suppress(KeyError):
             self.keys[f"{letter}{octave}"].activate()
 
     ###########################################################################
 
-    def release_key(self, letter: str, octave: int):
+    def release_key(self, letter: str, octave: int, offset: int = 0):
+        octave = max(min(octave + offset, 10), 0)
+
         with suppress(KeyError):
             self.keys[f"{letter}{octave}"].deactivate()
 
@@ -198,6 +236,18 @@ class Keyboard(QGraphicsView):
     @active.setter
     def active(self, val: bool) -> None:
         self._active = val
+
+    ###########################################################################
+
+    @property
+    def octave(self) -> int:
+        return self._octave
+
+    ###########################################################################
+
+    @octave.setter
+    def octave(self, val: int) -> None:
+        self._octave = max(min(val, 10), 0)
 
 
 ###############################################################################
@@ -226,10 +276,14 @@ def convert(ival: int) -> (str, int):
 
 
 def poll(keyboard: Keyboard) -> None:
-    fname = ""
+    in_fname = ""
+    out_fname = "beer"
 
-    with mido.open_input(fname) as inport:
+    with mido.open_input(in_fname) as inport, mido.open_output(
+        out_fname, virtual=True
+    ) as outport:
         for msg in inport:
+            outport.send(msg)
             if msg.type == "note_on":
                 keyboard.press_key(*convert(msg.note))
             elif msg.type == "note_off":
@@ -237,6 +291,6 @@ def poll(keyboard: Keyboard) -> None:
 
 
 thread = threading.Thread(target=poll, args=(keyboard,), daemon=True)
-thread.start()
+# thread.start()
 
 app.exec()
