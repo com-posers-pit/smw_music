73078b29fe19db8a39e8b081588168c86c9d3cc9
Thomas A. Werne <werneta@gmail.com>
2023-04-15 20:18:36 -0700

    Add more tests for N-SPC setpitch logic
    Now playing:
    Nothing for Me by Lupus Nocte, Zorro

--- smw_music/nspc.py
+++ smw_music/nspc.py
@@ -46,6 +46,23 @@ def set_pitch(tune: int, note: int, subnote: int = 0) -> int:
 
 
 c4 = 0xA4
+c5 = c4 + 12
+c6 = c5 + 12
 assert set_pitch(0x2F4, c4) == 0x62B
+assert set_pitch(0x2F4, c4 + 1) == 0x68A
+assert set_pitch(0x2F4, c4 + 2) == 0x6EE
+assert set_pitch(0x2F4, c4 + 3) == 0x759
+assert set_pitch(0x2F4, c4 + 4) == 0x7C9
+assert set_pitch(0x2F4, c4 + 5) == 0x83F
+assert set_pitch(0x2F4, c4 + 6) == 0x8BB
+assert set_pitch(0x2F4, c4 + 7) == 0x940
+assert set_pitch(0x2F4, c4 + 8) == 0x9CE
+assert set_pitch(0x2F4, c4 + 9) == 0xA64
+assert set_pitch(0x2F4, c4 + 10) == 0xB01
+assert set_pitch(0x2F4, c4 + 11) == 0xBA9
 assert set_pitch(0x2F4, c4 + 12) == 0xC5A
+
 assert set_pitch(0x3F4, c4) == 0x842
+
+assert set_pitch(0x2F4, c5) == 0xC5A
+assert set_pitch(0x2F4, c6) == 0x18C1
