1f9ae7c268c4073ea164657a27d1e2b2304890a9
Thomas A. Werne <werneta@gmail.com>
2023-04-15 18:31:39 -0700

    Reduce pitch conversion logic to something readable
    Now playing:
    Jacket by Mitch Murder

--- smw_music/nspc.py
+++ smw_music/nspc.py
@@ -20,101 +20,32 @@ _PITCH_TABLE = [
     0x10BE,
 ]
 
-
-def word(msb: int, lsb: int) -> int:
-    return (msb << 8) | lsb
+###############################################################################
 
 
-def set_pitch(
-    channel: int, r11: int, r10: int, tune: int, subtune: int
-) -> int:
-    stack = []
-    x = channel
-    stack.append(x)  # push x
-    a = r11  # mov    a, $11
-    c, a = divmod(2 * a, 256)  # asl    a
-    y = 0  # mov    y, #$00
-    x = 0x18  # mov    x, #$18
-    a, y = divmod(word(y, a), x)  # div    ya, x
-    x = a  # mov    x, a
-    a = _PITCH_TABLE[y] >> 8  # mov    a, PitchTable+1+y
-    r15 = a  # mov    $15, a
-    a = _PITCH_TABLE[y] & 0xFF  # mov    a, PitchTable+0+y
-    r14 = a  # mov    $14, a             ; set $14/5 from pitch table
-    a = _PITCH_TABLE[y + 1] >> 8  # mov    a, PitchTable+3+y
-    stack.append(a)  # push    a
-    a = _PITCH_TABLE[y + 1] & 0xFF  # mov    a, PitchTable+2+y
-    y = stack.pop()  # pop    y
-    y, a = divmod(word(y, a) - word(r15, r14), 256)  # subw    ya, $14
-    y = r10  # mov    y, $10
-    y, a = divmod(y * a, 256)  # mul    ya
-    a = y  # mov    a, y
-    y = 0  # mov    y, #$00
-    y, a = divmod(word(y, a) + word(r15, r14), 256)  # addw    ya, $14
-    r15 = y  # mov    $15, y
-    c, a = divmod(2 * a, 256)  # asl    a
-    c, r15 = divmod(2 * r15 + c, 256)  # rol    $15
-    r14 = a  # mov    $14, a
-    # bra    +
-    while x != 6:
-        # +    cmp    x, #$06
-        # bne    -
-        r15, c = divmod(r15, 2)  # -    lsr    $15
-        a, c = divmod(word(c, a), 2)  # ror    a
-        x += 1  # inc    x
+def set_pitch(tune: int, note: int, subnote: int = 0) -> int:
+    """
+    Reimplementation of SetPitch from main.asm
+    """
+    note = 0x7F & note
 
-    r14 = a  # mov    $14, a
-    x = stack.pop()  # pop    x
-    a = subtune  # mov    a, $02f0+x
-    y = r15  # mov    y, $15
-    y, a = divmod(y * a, 256)  # mul    ya
-    r17, r16 = y, a  # movw    $16, ya
-    a = subtune  # mov    a, $02f0+x
-    y = r14  # mov    y, $14
-    y, a = divmod(y * a, 256)  # mul    ya
-    stack.append(y)  # push    y
-    a = tune  # mov    a, $0210+x
-    y = r14  # mov    y, $14
-    y, a = divmod(y * a, 256)  # mul    ya
-    y, a = divmod(word(y, a) + word(r17, r16), 256)  # addw    ya, $16
-    r17, r16 = y, a  # movw    $16, ya
-    a = tune  # mov    a, $0210+x
-    y = r15  # mov    y, $15
-    y, a = divmod(y * a, 256)  # mul    ya
-    y = a  # mov    y, a
-    a = stack.pop()  # pop    a
-    y, a = divmod(word(y, a) + word(r17, r16), 256)  # addw    ya, $16
-    r17, r16 = y, a  #         movw    $16, ya
+    octave, idx = divmod(note, 12)
 
-    return word(r17, r16)
-    #    a = x  # mov    a, x               ; set voice X pitch DSP reg from $16/7
-    #    a = ((0x0F & a) << 4) | (
-    #        a >> 4
-    #    )  # xcn    a                 ;  (if vbit clear in $1a)
-    #    a, c = divmod(a, 2)  # lsr    a
-    #    a |= 2  #  or    a, #$02
-    #    y = a  #  mov    y, a               ; Y = voice X pitch DSP reg
-    #    a = r16  #         mov    a, $16
-    #    # call    DSPWriteWithCheck
-    #    y += 1  # inc    y
-    #    a = r17  # mov    a, $17
-    #    #                     ; write A to DSP reg Y if vbit clear in $1d
-    #    # DSPWriteWithCheck:
-    #    stack.append(a)  # push    a
-    #    a = r48  # mov    a, $48
-    #    a &= r1d  # and    a, $1d
-    #    if
-    #         pop    a
-    #         bne    +
-    #                     ; write A to DSP reg Y
-    #     DSPWrite:
-    #         mov    $f2, y
-    #         mov    $f3, a
-    #     +
-    #         ret
+    pitch = _PITCH_TABLE[idx]
+    next_pitch = _PITCH_TABLE[idx + 1]
+
+    delta = next_pitch - pitch
+
+    pitch += subnote * delta >> 8
+    pitch >>= 5 - octave
+
+    return pitch * tune >> 8
+
+
+###############################################################################
 
 
 c4 = 0xA4
-assert set_pitch(0, 0x7F & c4, 0, 0x2, 0xF4) == 0x62B
-assert set_pitch(0, 0x7F & (c4 + 12), 0, 0x2, 0xF4) == 0xC5A
-assert set_pitch(0, 0x7F & (c4), 0, 0x3, 0xF4) == 0x842
+assert set_pitch(0x2F4, c4) == 0x62B
+assert set_pitch(0x2F4, c4 + 12) == 0xC5A
+assert set_pitch(0x3F4, c4) == 0x842
