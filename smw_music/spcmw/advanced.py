81c3c5e682e4c4468dcb976aaf972c3d39b2e07d
Thomas A. Werne <werneta@gmail.com>
2024-02-17 12:22:26 -0800

    Add datatypes for advanced notation
    Now playing:
    DooM Dance (feat. Carpenter Brut, Gavin Rossdale) by Gunship, Carpenter Brut, Gavin Rossdale

---
+++ smw_music/spcmw/advanced.py
@@ -0,0 +1,143 @@
+# SPDX-FileCopyrightText: 2024 The SMW Music Python Project Authors
+# <https://github.com/com-posers-pit/smw_music/blob/develop/AUTHORS.rst>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+"""Advanced notation support"""
+
+###############################################################################
+# Imports
+###############################################################################
+
+# Standard library imports
+from dataclasses import dataclass
+from enum import IntEnum, auto
+
+###############################################################################
+# Imports
+###############################################################################
+
+
+class AdvType(IntEnum):
+    NOP = auto()
+    ECHO_FADE = auto()
+    GLISSANDO = auto()
+    GVOLUME_FADE = auto()
+    PAN_FADE = auto()
+    PITCH_BEND = auto()
+    PITCH_ENV_ATT = auto()
+    PITCH_ENV_REL = auto()
+    TREMOLO = auto()
+    VIBRATO = auto()
+    VOLUME_FADE = auto()
+
+
+###############################################################################
+
+
+@dataclass
+class Advanced:
+    name: str
+
+
+###############################################################################
+
+
+@dataclass
+class EchoFade(Advanced):
+    duration: int
+    final_volume: tuple[int, int]
+
+
+###############################################################################
+
+
+@dataclass
+class Gliassando(Advanced):
+    duration: int
+    semitones: int
+
+
+###############################################################################
+
+
+@dataclass
+class GVolumeFade(Advanced):
+    duration: int
+    semitones: int
+
+
+###############################################################################
+
+
+@dataclass
+class Nop(Advanced):
+    pass
+
+
+###############################################################################
+
+
+@dataclass
+class PanFade(Advanced):
+    duration: int
+    final_value: int
+
+
+###############################################################################
+
+
+@dataclass
+class PitchBend(Advanced):
+    delay: int
+    duration: int
+    offset: int
+
+
+###############################################################################
+
+
+@dataclass
+class PitchEnvAtt(Advanced):
+    delay: int
+    duration: int
+    semitones: int
+
+
+###############################################################################
+
+
+@dataclass
+class PitchEnvRel(Advanced):
+    delay: int
+    duration: int
+    semitones: int
+
+
+###############################################################################
+
+
+@dataclass
+class Tremolo(Advanced):
+    delay: int
+    duration: int
+    amplitude: int
+
+
+###############################################################################
+
+
+@dataclass
+class Vibrato(Advanced):
+    delay: int
+    duration: int
+    amplitude: int
+
+
+###############################################################################
+
+
+@dataclass
+class VolumeFade(Advanced):
+    duration: int
+    volume: int
